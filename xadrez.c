b904e2adac3ea7dbacb18d446b74b6168e823105
lu-an-zin <160800031+lu-an-zin@users.noreply.github.com>
2025-05-29 23:51:43 +0000

    Recursividade

--- xadrez.c
+++ xadrez.c
@@ -1,42 +1,66 @@
 #include<stdio.h>
 
+void movtorre(int torre ){
+    if(torre > 0){
+        printf("Direita\n");
+        movtorre(torre - 1);
+    }
+
+}
+
+void movbispo(int bispo ){
+    if(bispo > 0){
+        for(int i = 0; i < 1 ; i++){
+            printf("cima, ");
+            for(int j = 0; j < 1; j++){
+                printf("Direita\n");
+            }
+        }
+        movbispo(bispo - 1);
+    }
+}
+
+void movrainha(int rainha ){
+    if(rainha > 0){
+        printf("Esquerda\n");
+        movrainha(rainha - 1);
+    }
+
+}
+
+
+
 int main()
 {
 
     int vertical= 0;
     int horizontal = 2;
-    int torre = 1;
-    int bispo = 1;
-    int rainha = 1;
+    int torre = 5;
+    int bispo = 5;
+    int rainha = 8;
    // movimentação da torre
     printf("***Movimentação da torre***\n");
-    for(torre ; torre <=5; torre++){
-        printf("Direita\n");
-    }
+    movtorre(torre);
     
     // movimentação do Bispo
-    printf("***Movimentação do Bispo***\n");
-    
-    while(bispo <= 5){
-        printf("Cima, direita\n");
-        bispo++;
-    }
+    printf("***Movimentação do Bispo***\n"); 
+    movbispo(bispo);
+
     // movimentação da rainha
     
     printf("***Movimentação da rainha***\n");
-    do{
-        printf("Esquerda\n");
-        rainha ++;
-    }while(rainha <= 8 );
+    movrainha(rainha);
     
     // movimento do cavalo
-    for (horizontal; horizontal <= 2; horizontal++)
-    {
-        for(vertical; vertical <= 1; vertical++){
-            printf("Baixo\n");
-        }
-        printf("Esquerda\n");
+    printf("***Movimentação do cavalo***\n");
+    
+    for (int i = 0, j = 2; i < 3; i++, j--) {
+    if (i < 2) {
+        printf("Cima\n");
+    } else {
+        printf("Direita\n");
     }
+}
     
     return 0;
 }
